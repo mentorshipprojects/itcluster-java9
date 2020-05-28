5cd0a27f61858604530ce147076388094a5e925f
Otaku-First <52919535+Otaku-First@users.noreply.github.com>
2020-05-28 14:24:24 +0300

    Added a password authentication script

--- src/main/java/forest/detector/utils/HTMLTemplates.java
+++ src/main/java/forest/detector/utils/HTMLTemplates.java
@@ -32,7 +32,7 @@ script().withSrc("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/
                                                       div(
                                                               img()
                                                                       .attr("alt","image")
-                                                              .withSrc("/img/no-ava.png")
+                                                                      .withSrc("https://i.pinimg.com/originals/ff/a0/9a/ffa09aec412db3f54deadf1b3781de2a.png")
                                                       ).withClass("nav-profile-img")
                                               ).withClass("nav-link")
                                                       .withId("profileDropdown")
@@ -187,7 +187,7 @@ div(i().withClass("fa fa-chevron-up")).withClass("scrollup"),
                               div(
                                       a(
                                               i().withClass("fab fa-telegram").withStyle("color: #007bff;")
-                                      ).withHref("https://t.me/Java9ProjectBot")
+                                      ).withHref("#")
                                               .withClass("tg-link")
                                       .attr("data-toggle","popover")
                                       .attr("data-placement","top")
