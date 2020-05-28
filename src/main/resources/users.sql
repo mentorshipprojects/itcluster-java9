72ed4c0432516560fb1e957a0b00aeebd2dc0ee2
Viktor Kukuruza <kukuruzavictor@gmail.com>
2020-05-28 18:56:50 +0300

    Created UPDATE feature and COUNTER of parsing process

--- src/main/resources/users.sql
+++ src/main/resources/users.sql
@@ -1,33 +1,42 @@
+    create schema forest collate utf8_general_ci;
 
--- user management
-create table users (
-  USER_NAME           varchar(15) not null primary key,
-  USER_PASSWD         varchar(15) not null
-);
-insert into users (USER_NAME, USER_PASSWD) values('moderator','moderator');
-insert into users (USER_NAME, USER_PASSWD) values('admin','admin');
+--postgreSQL
 
-create table user_roles (
-  USER_NAME         varchar(15) not null,
-  ROLE_NAME         varchar(15) not null,
-  primary key (USER_NAME, ROLE_NAME)
-);
-insert into user_roles (USER_NAME, ROLE_NAME) values('admin','admin');
-insert into user_roles (USER_NAME, ROLE_NAME) values('admin','moderator-api');
-insert into user_roles (USER_NAME, ROLE_NAME) values('admin','moderator-gui');
-insert into user_roles (USER_NAME, ROLE_NAME) values('moderator','moderator-api');
-insert into user_roles (USER_NAME, ROLE_NAME) values('moderator','moderator-gui');
-
--- media content
-
--- sessions
-create table tomcat_sessions
+create table tickets
 (
-    session_id    varchar(100) not null primary key,
-    valid_sessions char(1)      not null,
-    max_inactive  int          not null,
-    last_access   bigint       not null,
-    app_name      varchar(255),
-    session_data  bytea
+	id serial not null
+		constraint tickets_pk
+			primary key,
+	number varchar(20) not null,
+	region varchar(100) not null,
+	forest_user varchar(100) not null,
+	start_date date not null,
+	finish_date date not null,
+	forestry varchar(100),
+	cutting_type varchar(100),
+	ticket_status varchar(20),
+	cutting_status varchar(20)
 );
-CREATE INDEX kapp_name ON tomcat_sessions USING hash (app_name);
+
+
+create table tracts
+(
+	id serial not null
+		constraint tracts_pk
+			primary key,
+	ticket_id integer not null,
+	quarter varchar(20),
+	division varchar(20),
+	range varchar(20),
+	area real,
+	forest_type varchar(20),
+	general_allowed_extent double precision,
+	allowed_extent double precision,
+	cutting_status varchar(20),
+	contributor varchar(100),
+	map_id varchar(20)
+);
+
+create unique index tracts_map_id_uindex
+	on tracts (map_id);
+
